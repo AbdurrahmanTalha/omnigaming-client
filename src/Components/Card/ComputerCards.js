5bf682b1f38d1c8efc5cd177a6b79155321fb80f
AbdurrahmanTalha <66727914+AbdurrahmanTalha@users.noreply.github.com>
2022-04-29 09:56:49 +0600

    finished home and cards functionalty left only

--- src/Components/Card/ComputerCards.js
+++ src/Components/Card/ComputerCards.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 
 const ComputerCards = computer => {
-    console.log(computer);
     return (
         <div className="col-md-4 my-5">
             <Card >
                 <Card.Img variant="top" src={computer.computer.img} />
                 <Card.Body>
                     <Card.Title>{computer.computer.name}</Card.Title>
-                    <Button variant="primary">Deliver</Button>
+                    <p>Desc: {computer.computer.desc}</p>
+                    <p>${computer.computer.price}</p>
+                    <p>Quantity: {computer.computer.quantity}</p>
+                    <p>Manufacturer: {computer.computer.supplier}</p>
+                    <Button variant="primary">Update</Button>
                 </Card.Body>
             </Card>
         </div>
--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -8,12 +8,12 @@ const Cards = () => {
             .then(res => res.json())
             .then(data => setComputers(data))
     }, [])
-    console.log(computers);
     return (
         <div className="row mx-auto d-flex align-items-center justify-content-center my-5">
             {
                 computers.map(computer => <ComputerCards computer={computer} key={computer._id}></ComputerCards>)
             }
+            
         </div>
     );
 };
