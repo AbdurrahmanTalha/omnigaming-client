71f056e2e08453a33acf52b85abd4be1a705279f
AbdurrahmanTalha <66727914+AbdurrahmanTalha@users.noreply.github.com>
2022-05-02 03:34:40 +0600

    added responsive navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Link } from 'react-router-dom';
 import auth from '../../firebase.init';
 import "./Navbar.css"
@@ -12,52 +13,44 @@ const Header = () => {
     const handleSignOut = () => {
         signOut(auth)
     }
-    // <nav className="navbar d-flex align-items-center mb-0">
-    //     <div className="logo">
-    //         <h2><span className="text-info">O</span>mniGaming</h2>
-    //     </div>
-    //     <ul className="nav-items d-flex">
-    //         <li className="nav-item"><Link to="/">Home</Link></li>
-    //         {
-    //             user && <>
-    //                 <li className="nav-item"><Link to="/addItem">Add Item</Link></li>
-    //                 <li className="nav-item"><Link to="/inventory">Manage Items</Link></li>
-    //                 <li className="nav-item"><Link to="/myItems">My Items</Link></li>
-    //             </>
-    //         }
-    //         <li className="nav-item"><Link to="/blogs">Blogs</Link></li>
-    //         <li>{
-    //             user ?
-    //                 <button className="btn btn-primary" onClick={handleSignOut}>SignOut</button>
-    //                 :
-    //                 <Link className="btn btn-primary" to="/login">Login</Link>
-    //         }
-    //         </li>
-    //     </ul>
-    // </nav>
+    
     return (
-        <Navbar bg="dark" variant="dark">
+     
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="/">OmniGaming</Navbar.Brand>
-                <Nav className="d-flex justify-content-between align-items-center">
-                    <li className="nav-item"><Link to="/">Home</Link></li>
+                <LinkContainer to="/">
+                    <Navbar.Brand href="/">OmniGaming</Navbar.Brand>
+                </LinkContainer>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto">
+                        <LinkContainer to="/">
+                            <Nav.Link>Home</Nav.Link>
+                        </LinkContainer>
+                        {
+                            user && <>
+                                <LinkContainer to="/addItem">
+                                    <Nav.Link>Add Item</Nav.Link>
+                                </LinkContainer>
+                                <LinkContainer to="/inventory">
+                                    <Nav.Link>Inventory</Nav.Link>
+                                </LinkContainer>
+                                <LinkContainer to="/myItems">
+                                    <Nav.Link>My Items</Nav.Link>
+                                </LinkContainer>
+                            </>
+                        }
+                        <LinkContainer to="/blogs">
+                            <Nav.Link>Blogs</Nav.Link>
+                        </LinkContainer>
+                        {
+                            user ?
+                                <button className="btn btn-primary" onClick={handleSignOut}>SignOut</button> : <Link className="btn btn-primary" to="/login">Login</Link>
+                        }
 
-                    {
-                        user && <>
-                            <li className="nav-item"><Link to="/addItem">Add Item</Link></li>
-                            <li className="nav-item"><Link to="/inventory">Manage Items</Link></li>
-                            <li className="nav-item"><Link to="/myItems">My Items</Link></li>
-                        </>
-                    }
-                    <li className="nav-item"><Link to="/blogs">Blogs</Link></li>
-                    <li>{
-                        user ?
-                            <button className="btn btn-primary" onClick={handleSignOut}>SignOut</button>
-                            :
-                            <Link className="btn btn-primary" to="/login">Login</Link>
-                    }
-                    </li>
-                </Nav>
+                    </Nav>
+
+                </Navbar.Collapse>
             </Container>
         </Navbar>
 
